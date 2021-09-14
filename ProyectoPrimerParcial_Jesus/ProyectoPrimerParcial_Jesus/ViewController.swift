05e7e63ccf631a6952a1be747b47595ae5c2c40c
Jesús <51033376+Jesus0414@users.noreply.github.com>
2021-09-13 17:21:14 -0700

    primer cambio

---
+++ ProyectoPrimerParcial_Jesus/ProyectoPrimerParcial_Jesus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoPrimerParcial_Jesus
+//
+//  Created by Alumno on 9/13/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
